86090586a591b46164742ac515b37f9599fe2c88
هخه <44414487+A7MEDA7MED@users.noreply.github.com>
2019-02-15 05:46:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on('ready', () => {
 
 client.on('ready', () => {
     console.log(`${client.user.tag} Is idle !`) 
-    client.user.setGame(`Royal Legend Clan . 6help`)
+    client.user.setGame(`! Hydrogen . 6help`)
 });
 
 
